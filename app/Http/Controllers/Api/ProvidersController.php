5a07d681028ebf3b05593bfafd90ab214624f77b
Ali Moradi <Developer.n.c@gmail.com>
2020-05-18 12:01:42 +0430

    delete document before insert

--- app/Http/Controllers/Api/ProvidersController.php
+++ app/Http/Controllers/Api/ProvidersController.php
@@ -81,7 +81,7 @@ class ProvidersController extends Controller
         $name = uniqid("", true).'.'.$extension;
         Storage::putFileAs($directory,$request->file('file'),$name);
         $providerId = Provider::where('user_id',auth()->user()->id )->first()->id;
-        $document = ProviderVerificationDocument::where("provider_id", $providerId)->where('title', $title)->first();
+        $document = ProviderVerificationDocument::where("provider_id", $providerId)->where('title', $title)->get();
         if($document)
         {
             $document->delete();
