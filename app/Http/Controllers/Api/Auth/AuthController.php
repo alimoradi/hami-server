acb65540706e77111358b21fe9cbcc80ab1d982e
Ali Moradi <Developer.n.c@gmail.com>
2021-02-21 00:08:14 +0330

    fixed error

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -246,7 +246,7 @@ class AuthController extends Controller
             if ($saveResult) {
 
                 Affiliation::confirmAffiliation($user->id);
-                if (auth()->attempt([$user->phone, $user->password])) {
+                if (auth()->attempt([['phone' => $user->phone, 'password' => $user->password]])) {
 
 
 
