04431b144e202ff7b480646fbc2f64082cfb2287
Ali Moradi <Developer.n.c@gmail.com>
2021-08-08 14:51:19 +0430

    changed code in discount to string

--- database/migrations/2021_08_08_100741_change_code_in_discounts.php
+++ database/migrations/2021_08_08_100741_change_code_in_discounts.php
@@ -14,6 +14,7 @@ class ChangeCodeInDiscounts extends Migration
     public function up()
     {
         Schema::table('discounts', function (Blueprint $table) {
+            $table->dropUnique('discounts_code_unique');
             $table->string('code',255)->change();
 
         });
